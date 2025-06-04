41330618de8824e367a91bc81a373b47bea23de4
Luiz Carlos <junior1232131@gmail.com>
2025-06-04 17:59:27 -0300

    fix: improve HWID retrieval by using PowerShell commands for motherboard UUID and disk serial number

--- backend/pkg/hwid/hwid.go
+++ backend/pkg/hwid/hwid.go
@@ -3,6 +3,8 @@ package hwid
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
+	"strings"
 
 	"github.com/hex-boost/hex-nexus-app/backend/pkg/command"
 	"github.com/hex-boost/hex-nexus-app/backend/pkg/sysquery"
@@ -21,18 +23,30 @@ func New() *HWID {
 }
 
 func (u *HWID) Get() (string, error) {
-	output, err := u.command.Execute("cmd.exe", "/c", "wmic csproduct get uuid")
+	motherboardUUIDCmd := `(Get-CimInstance Win32_ComputerSystemProduct).UUID`
 
-	//output, err := u.sysquery.GetHardwareUUID()
+	diskSerialCmd := `$diskSerial = (Get-CimInstance Win32_DiskDrive | Where-Object {$_.SerialNumber -ne $null -and $_.SerialNumber.Trim() -ne ''} | Select-Object -First 1).SerialNumber; if ($diskSerial) { $diskSerial.Trim() } else { "" }`
+
+	motherboardUUIDOutput, err := u.command.Execute("powershell.exe", "-NoProfile", "-Command", motherboardUUIDCmd)
 	if err != nil {
-		return "", err
+		fmt.Println("Error getting Motherboard UUID:", err)
+	}
+	motherboardUUID := strings.TrimSpace(string(motherboardUUIDOutput))
+
+	diskSerialOutput, err := u.command.Execute("powershell.exe", "-NoProfile", "-Command", diskSerialCmd)
+	if err != nil {
+		fmt.Println("Error getting Disk Serial Number:", err)
+	}
+	diskSerial := strings.TrimSpace(string(diskSerialOutput))
+
+	combinedOutput := motherboardUUID + diskSerial
+
+	if combinedOutput == "" {
+		return "", fmt.Errorf("failed to retrieve any hardware identifiers (Motherboard UUID or Disk Serial)")
 	}
 
-	out := string(output)
-
-	// Hash the output
 	hasher := sha256.New()
-	hasher.Write([]byte(out))
+	hasher.Write([]byte(combinedOutput))
 	hash := hex.EncodeToString(hasher.Sum(nil))
 	return hash, nil
 }
